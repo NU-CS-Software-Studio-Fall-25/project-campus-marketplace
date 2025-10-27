d04fcb0882750352073935856876ac38a566aea9
Aryaman Chawla <aryamanchawla2025@u.northwestern.edu>
2025-10-26 20:54:05 -0500

    fix permissions

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   resource :profile, only: %i[show update]
   # Allow full CRUD for users so system tests and app pages can access index/show/edit/update/destroy
-  resources :users, only: %i[new create]
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
