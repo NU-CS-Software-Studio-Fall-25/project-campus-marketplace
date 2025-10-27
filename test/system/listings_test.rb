4b95cb006bf6d2a68fa383e600de09ca44900af1
Ishaan Jalan <ishaan.jalan04@gmail.com>
2025-10-26 19:10:01 -0500

    fix failing test

--- test/system/listings_test.rb
+++ test/system/listings_test.rb
@@ -3,42 +3,43 @@ require "application_system_test_case"
 class ListingsTest < ApplicationSystemTestCase
   setup do
     @listing = listings(:one)
+    @user = users(:one)
   end
 
   test "visiting the index" do
+    sign_in_as(@user)
     visit listings_url
-    assert_selector "h1", text: "Listings"
+    assert_selector "h1", text: "All Listings"
   end
 
   test "should create listing" do
-    visit listings_url
+    sign_in_as(@user)
+    visit mine_listings_url
     click_on "New listing"
 
     fill_in "Description", with: @listing.description
     fill_in "Price", with: @listing.price
     fill_in "Title", with: @listing.title
-    fill_in "User", with: @listing.user_id
     click_on "Create Listing"
 
     assert_text "Listing was successfully created"
-    click_on "Back"
   end
 
   test "should update Listing" do
+    sign_in_as(@user)
     visit listing_url(@listing)
     click_on "Edit this listing", match: :first
 
-    fill_in "Description", with: @listing.description
+    fill_in "Description", with: "Updated description"
     fill_in "Price", with: @listing.price
     fill_in "Title", with: @listing.title
-    fill_in "User", with: @listing.user_id
     click_on "Update Listing"
 
     assert_text "Listing was successfully updated"
-    click_on "Back"
   end
 
   test "should destroy Listing" do
+    sign_in_as(@user)
     visit listing_url(@listing)
     click_on "Destroy this listing", match: :first
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,9 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+class ActionDispatch::IntegrationTest
+  def sign_in_as(user, password: "password")
+    post session_path, params: { email_address: user.email_address, password: password }
+  end
+end
