cccb5a27e311885f59b9d706f9e81e88ce18cce7
aaronkazii2 <aaronkazi2027@u.northwestern.edu>
2025-11-03 17:46:57 -0600

    fix database

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_10_27_235959) do
-  create_schema "_heroku"
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_stat_statements"
