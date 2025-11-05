76f38ff17242a0af852caf9dd30a49ffd5d92a64
Ishaan Jalan <ishaan.jalan04@gmail.com>
2025-11-04 22:28:24 -0600

    fix issue

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
-import { application } from "./application"
-import HelloController from "./hello_controller"
-import ListingsSearchController from "./listings_search_controller"
+import { application } from "controllers/application"
+import HelloController from "controllers/hello_controller"
+import ListingsSearchController from "controllers/listings_search_controller"
 
 application.register("hello", HelloController)
 application.register("listings-search", ListingsSearchController)
