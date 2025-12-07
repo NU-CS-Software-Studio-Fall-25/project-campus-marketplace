e743e11fbfcaeaad426e7704d488747e4150b40f
Aryaman Chawla <aryamanchawla2025@u.northwestern.edu>
2025-12-07 14:43:44 -0600

    fix failing linter

--- spec/system/favorites_spec.rb
+++ spec/system/favorites_spec.rb
@@ -23,10 +23,10 @@ RSpec.describe "Favorites", type: :system do
 
   it "allows a user to add a listing to favorites" do
     sign_in_as(user)
-    
+
     # Add favorite directly
     user.favorites.create!(listing: listing)
-    
+
     # Verify it appears in favorites index
     visit favorites_path
     expect(page).to have_text(listing.title, wait: 5)
@@ -34,13 +34,13 @@ RSpec.describe "Favorites", type: :system do
 
   it "allows a user to remove a listing from favorites" do
     sign_in_as(user)
-    
+
     # First add to favorites
     favorite = user.favorites.create!(listing: listing)
-    
+
     # Remove it
     favorite.destroy
-    
+
     # Verify it's removed from database
     user.reload
     expect(user.favorites.where(listing: listing).exists?).to be false
@@ -48,10 +48,10 @@ RSpec.describe "Favorites", type: :system do
 
   it "displays favorited listings in the favorites index" do
     sign_in_as(user)
-    
+
     # Add listing to favorites
     user.favorites.create!(listing: listing)
-    
+
     visit favorites_path
 
     expect(page).to have_text(listing.title, wait: 5)
@@ -60,7 +60,7 @@ RSpec.describe "Favorites", type: :system do
 
   it "shows empty state when user has no favorites" do
     sign_in_as(user)
-    
+
     visit favorites_path
 
     expect(page).to have_text("No listings yet", wait: 5)
