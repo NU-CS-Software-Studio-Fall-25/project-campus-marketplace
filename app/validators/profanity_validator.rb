ec618bead0f6736f0f9341fae2bbdc49b23360a7
aaronkazii2 <aaronkazi2027@u.northwestern.edu>
2025-12-08 10:27:09 -0600

    fixing profanity checker

--- app/validators/profanity_validator.rb
+++ app/validators/profanity_validator.rb
@@ -1,8 +1,18 @@
 class ProfanityValidator < ActiveModel::EachValidator
+  BLACKLIST = (Obscenity::Base.blacklist || []).map { |w| w.to_s.strip }.reject(&:empty?)
+
   def validate_each(record, attribute, value)
     return if value.blank?
-    return unless Obscenity.profane?(value)
+    return unless profane_whole_word?(value)
 
     record.errors.add(attribute, options[:message] || "contains inappropriate language")
   end
+
+  private
+    def profane_whole_word?(text)
+      BLACKLIST.any? do |term|
+        pattern = /\b#{Regexp.escape(term)}\b/i
+        text.match?(pattern)
+      end
+    end
 end
